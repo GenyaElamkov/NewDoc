a73322a6dbd436e24f6970b9d84b6e5e7d88e275
GenyaElamkov <GenyaElamkov@gmail.com>
2023-03-14 15:53:09 +0300

    [upd] - first commit

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 """
-Скрипт создает копию word документа с тукущей датой.
+Скрипт создаёт копию word документа с текущей датой.
 """
+import glob
+from datetime import datetime
+
+from docx import Document
+
+
+def create_copy_docx(start_file: str) -> None:
+    doc = Document(docx=start_file)
+
+    pattern = '%d.%m.%Y'
+    dt_now = datetime.now().strftime(pattern)
+
+    doc.save(f'{dt_now}.docx')
+
+
+def main():
+    try:
+        names_docs = sorted(glob.glob('*.docx'))[-1]
+        create_copy_docx(start_file=names_docs)
+    except IndexError:
+        print('[!] No file')
+        print()
+
+
+if __name__ == '__main__':
+    main()
